ddae5ee2a6f19c02dbcda02726e9506ec7bb90de
Igor Napolov <isnapolov@gmail.com>
2021-11-15 13:00:18 +0200

    Задание 1 - галерея изображений

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const imagesList = galleryItems.map(image =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+    <img
+      class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+  </a>
+</div>`
+).join("");
+
+
+galleryEl.insertAdjacentHTML("beforeend", imagesList);
+
+
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+    return
+    };
+    const largeImage = evt.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+    <img src="${largeImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            instance.close()
+        }
+    });    
+    
+}
+
+
+
+
